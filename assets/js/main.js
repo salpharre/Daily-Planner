4339815112db8c931e88f52ac6a6ee49e481c7e2
Sandra <sandra.alpha2010@gmail.com>
2020-04-29 21:00:34 -0700

    got rid of seperate cols for time, textarea, and save button and put it all side by side in one row. brainstorming for script currently all commentted out. changed a class name referenced in style css to reflect class change in index html

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+//to get save button to save what's in textarea to local storage
+
+//click event with function that has:
+//preventdefault
+//document.queryselect("textarea").value = varName
+//localStorage.setItem("text", varName)
+
+//later to retrieve
+//var savedText = localStorage.getItem("text")
+
+
+//toggling between 24hrs and am/pm
+//var hourSwitcher = document.querySelector("#customSwitch1");
+//var hourDiv = document.querySelector(".hour");
+
+//var timeFormat = "amPm"
+
+//hourSwitcher.addEventListener("click", function(){
+//   if (timeFormat === "amPm"){
+        //timeFormat === "military"; //or put moment() here
+        //$hourDiv.(jquery method of setAttribute)()
+    //}
+// })
+
+
+//varName = moment()
+
+//instead of setting text in the hour div when changed to am/pm to say am/pm
+//you can call moment to bring that too
+
+//Logic for changing colors in real-time
+
+//should each text area have it's own class name to refer to to change?
+
+//pure conditional logic
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
