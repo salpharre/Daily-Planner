fa1c7bf996928708b78e839c3e6459de62c46cf3
Sandra <sandra.alpha2010@gmail.com>
2020-05-02 04:10:35 -0700

    created a function that should work, troubleshooting to see what to change

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,7 @@ var todayPlanner =[];
 
 var amPm = ["12 am", "1 am", "2 am", "3 am", "4 am", "5 am", "6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm", "9 pm", "10 pm", "11 pm"];
 
+var momentAmPm = []
 //for a future feature, toggling between am/pm and 24hr time format
 // var twentyFour = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"];
 
@@ -29,6 +30,8 @@ function populateObject(){
    for (var i = 0; i < amPm.length; i++){
 
     var hourToPlanner = {hour: amPm[i], text: ""};
+    //put a moment in hour
+
 
     todayPlanner.push(hourToPlanner);
     //console.log(todayPlanner);
@@ -88,8 +91,10 @@ todaysDate.innerHTML = dateMoment;
 
 //use moment for color-changing
 
-var fromNowPast = moment().startOf("day").fromNow()
-var fromNowFuture = moment().endOf("day").fromNow()
+var startOfDayHour = moment().startOf("day").fromNow()
+var endOfDayHour = moment().endOf("day").fromNow()
+
+
 
 var hourMoment = moment().format("h");
 
@@ -104,16 +109,21 @@ var momentAmPm = [];
 // }console.log(momentAmPm);
 
 function movingColors(){
-    
-    for (var i = 0; i < momentAmPm.length; i++){
 
-        if (i === fromNowPast){
+    //run through created array of objects to
+    //trying to get the moment to check againt the hour in the array of objects
+    
+    //might work if can change the 
+
+    for (var i = 0; i < todayPlanner.length; i++){
+
+        if (todayPlanner[i].hour === (hourMoment.subtract("hours", 1))){
             textareaE1.setAttribute("style", "background: grey")
         }
-        else if (i === fromNowFuture){
+        else if (todayPlanner[i].hour === (hourMoment.add("hours", 1))){
             textareaE1.setattribute("style", "background: blue")
         }
-        else if (i !== fromNowPast && i !== fromNowFuture){
+        else if (todayPlanner[i].hour === hourMoment){
             textareaE1.setattribute("style", "background: yellow")
         } 
     }
