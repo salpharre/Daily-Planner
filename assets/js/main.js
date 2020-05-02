c9ebfe4aecc19afda3b6b0331e43ee137b4a8495
Sandra <sandra.alpha2010@gmail.com>
2020-05-02 09:00:42 -0700

    pre-filled out array of objects so to use the hour property for use in a functino later for moment js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,41 +5,128 @@ var textArea = document.querySelectorAll(".description");
 var textareaE1 = document.querySelectorAll("textarea");
 
 
+
+
 // let body = document.getElementsByTagName("body")[0];
 
 // var comeBackDate = localStorage.getItem("hour");
 
-//retrieve info from local storage
+
+//located here so the date is updated first
+currentDay();
 
 var parseTodayPlanner = JSON.parse(localStorage.getItem("text"));
-console.log(parseTodayPlanner);
+//console.log(parseTodayPlanner);
 
-var todayPlanner =[];
+var todayPlanner =[
+    {
+        hour: "00:00",
+        text: ""
+    },
+    {
+        hour: "01:00",
+        text: ""
+    },
+    {
+        hour: "02:00",
+        text: ""
+    },
+    {
+        hour: "03:00",
+        text: ""
+    },
+    {
+        hour: "04:00",
+        text: ""
+    },
+    {
+        hour: "05:00",
+        text: ""
+    },
+    {
+        hour: "06:00",
+        text: ""
+    },
+    {
+        hour: "07:00",
+        text: ""
+    },
+    {
+        hour: "08:00",
+        text: ""
+    },
+    {
+        hour: "09:00",
+        text: ""
+    },
+    {
+        hour: "10:00",
+        text: ""
+    },
+    {
+        hour: "11:00",
+        text: ""
+    },
+    {
+        hour: "12:00",
+        text: ""
+    },
+    {
+        hour: "13:00",
+        text: ""
+    },
+    {
+        hour: "14:00",
+        text: ""
+    },
+    {
+        hour: "15:00",
+        text: ""
+    },
+    {
+        hour: "16:00",
+        text: ""
+    },
+    {
+        hour: "17:00",
+        text: ""
+    },
+    {
+        hour: "18:00",
+        text: ""
+    },
+    {
+        hour: "19:00",
+        text: ""
+    },
+    {
+        hour: "20:00",
+        text: ""
+    },
+    {
+        hour: "21:00",
+        text: ""
+    },
+    {
+        hour: "22:00",
+        text: ""
+    },
+    {
+        hour: "23:00",
+        text: ""
+    }
+];
 
-var amPm = ["12 am", "1 am", "2 am", "3 am", "4 am", "5 am", "6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm", "9 pm", "10 pm", "11 pm"];
+//var amPm = ["12 am", "1 am", "2 am", "3 am", "4 am", "5 am", "6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm", "9 pm", "10 pm", "11 pm"];
 
-var momentAmPm = []
+
+//placed call here because the array of objects had just been created
+movingColors();
+
 //for a future feature, toggling between am/pm and 24hr time format
 // var twentyFour = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"];
 
 
-/*populate empty array todayPlanner with strings from the the amPm array
-to create an object for every timeblock*/
-function populateObject(){
-
-   for (var i = 0; i < amPm.length; i++){
-
-    var hourToPlanner = {hour: amPm[i], text: ""};
-    //put a moment in hour
-
-
-    todayPlanner.push(hourToPlanner);
-    //console.log(todayPlanner);
-
-   
-}
-}
-populateObject();
 
 //trying to change the text value of each object in the array
 //console.log comes back with undefined for text: key
@@ -58,6 +145,7 @@ function populateObjectWithText(){
         }
     }
 }
+//console.log(todayPlanner);
 
 
 //Click Event for Save Button
@@ -66,7 +154,7 @@ function saveText(){
     saveButton.addEventListener("click", function(event){
         event.preventDefault();
 
-       // populateObjectWithText();   
+        populateObjectWithText();   
 
     });
     
@@ -82,53 +170,67 @@ function saveText(){
 //initilize moment using LL format of date
 //grab id of div where date will go
 //print the moment onto webpage
-
+function currentDay(){
 var dateMoment = moment().format("LL");
 
 var todaysDate = document.getElementById("currentDay");
 
 todaysDate.innerHTML = dateMoment;
+}
 
 //use moment for color-changing
 
+var hourMoment = moment();
+
+var hourSubtract = hourMoment.subtract(1, "hours")
+
+var hourAdd = hourMoment.add(1, "hours");
+
+
+
 var startOfDayHour = moment().startOf("day").fromNow()
 var endOfDayHour = moment().endOf("day").fromNow()
-
-
-
-var hourMoment = moment().format("h");
-
-var momentAmPm = [];
-
-//won't work because it'll replace every string with the same hour
-// function changeHour(){
-// for (var i = 0; i < amPm.length; i++){
-//     var replaceHour = amPm[i].replace(amPm[i], hourMoment);
-//     momentAmPm.push(replaceHour);
-// }
-// }console.log(momentAmPm);
 
 function movingColors(){
 
     //run through created array of objects to
     //trying to get the moment to check againt the hour in the array of objects
     
-    //might work if can change the 
+    //might work if can change the todayPlanner.hour to a moment
 
-    for (var i = 0; i < todayPlanner.length; i++){
+    //can i make it work?
 
-        if (todayPlanner[i].hour === (hourMoment.subtract("hours", 1))){
-            textareaE1.setAttribute("style", "background: grey")
-        }
-        else if (todayPlanner[i].hour === (hourMoment.add("hours", 1))){
-            textareaE1.setattribute("style", "background: blue")
-        }
-        else if (todayPlanner[i].hour === hourMoment){
-            textareaE1.setattribute("style", "background: yellow")
-        } 
-    }
+    
+
+    // m = moment().format("00").isBefore("23");
+    // console.log(m);
+    var m = moment()
+
+    //if (m = moment()
+
+
+
+
+    // for (var m = moment(firstMoment); m.isBefore(lastMoment); m.add(1, "hour")){
+    //     console.log(m.format("HH:mm"));
+    // }
+
+
+    // for (var i = 0; i < todayPlanner.length; i++){
+
+        
+    //     if (todayPlanner[i].hour === hourSubtract){
+    //         textareaE1.setAttribute("style", "background: grey")
+    //     }
+    //     else if (todayPlanner[i].hour === hourAdd){
+    //         textareaE1.setattribute("style", "background: blue")
+    //     }
+    //     else if (todayPlanner[i].hour === hourMoment){
+    //         textareaE1.setattribute("style", "background: yellow")
+    //     } 
+    
 }
-movingColors();
+//movingColors();
 
 
 
